b8a334ec68ed1eb2d63cd0619177545b9f38504f
swier <swier.garst@gmail.com>
2021-09-23 11:10:15 +0200

    more trying on figuring out the output

--- v6_fedPCA_py/__init__.py
+++ v6_fedPCA_py/__init__.py
@@ -22,8 +22,8 @@ def RPC_get_cov_mat(data):
     num_cols = data.drop(columns = ['test/train', 'label']).values.shape[1]
     f = tb.open_file('tmp.h5', 'w')
     filters = tb.Filters(complevel=5, complib='blosc')
-    result = f.create_carray(f.root, 'data', tb.Float32Atom(), shape=(num_cols, num_cols), filters=filters)
-    result = data.drop(columns = ['test/train', 'label']).values
+    result = f.create_carray(f.root, 'data', tb.Float32Atom(), shape=(num_rows, num_cols), filters=filters)
+    result[:,:] = data.drop(columns = ['test/train', 'label']).values
 
     '''
     for i in range(num_cols):
@@ -39,4 +39,4 @@ def RPC_get_cov_mat(data):
 
 
 
-    return out
+    return out[:,:]
